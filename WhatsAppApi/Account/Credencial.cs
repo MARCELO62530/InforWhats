3381907be471653ee4c866fb849f2bf2fbae9ede
Marcelo San'Ana Junior <Marcelo@perfeccio.com.br>
2014-12-30 22:34:01 -0200

    Commit

---
+++ WhatsAppApi/Account/Credencial.cs
@@ -0,0 +1,15 @@
+﻿namespace WhatsAppApi.Account
+{
+  public  class Credencial:Pessoa
+    {
+        public string Senha { get; set; }
+        public override string ToString()
+        {
+            return Telefone;
+        }
+
+        public bool Debug { get; set; }
+
+        public bool Hidden { get; set; }
+    }
+}
